f7ec52aa3c00dd6a641eb86b1e18b5d534242842
Andrew Dunn <atd2005@gmail.com>
2026-02-18 15:49:26 -0600

    fix deploy account

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -11,7 +11,7 @@ const repoName = "andrewtdunn/aws1.1_document-processor";
 const branch = "main";
 const connectionArn =
   "arn:aws:codeconnections:us-east-1:637423577773:connection/5aaaaa13-84f7-403e-b14f-a3b92040799e";
-const prodAccount: string = "471112703167";
+const prodAccount: string = "219765858109";
 const deployRegion = "us-east-1";
 
 export class PipelineStack extends cdk.Stack {
