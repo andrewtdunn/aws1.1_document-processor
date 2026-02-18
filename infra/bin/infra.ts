44b8822fcfe514cec529488e02c7fe10f7ebd79b
Andrew Dunn <atd2005@gmail.com>
2026-02-18 01:01:01 -0600

    add pipeline

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib/core";
 import { InfraStack } from "../lib/infra-stack";
+import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
-new InfraStack(app, "InfraStack", {
+new PipelineStack(app, "PipelineStack", {
   env: { account: "637423577773", region: "us-east-1" },
 });
 
