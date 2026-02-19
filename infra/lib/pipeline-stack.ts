8960b8b30a954a092b2386523317221ce6c113bc
Andrew Dunn <atd2005@gmail.com>
2026-02-18 20:52:13 -0600

    change pipeline name

--- infra/lib/pipeline-stack.ts
+++ infra/lib/pipeline-stack.ts
@@ -19,7 +19,7 @@ export class PipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, "Pipeline", {
-      pipelineName: "document-processor-pipeline",
+      pipelineName: "claims-processor-pipeline",
       crossAccountKeys: true,
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.connection(repoName, branch, {
