e1aa2471a9a3a9400dea5450a3bdcf9aec99c4b0
Andrew Dunn <atd2005@gmail.com>
2026-04-01 04:05:00 -0500

    add vpce

--- infra/lib/networking-stack.ts
+++ infra/lib/networking-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Stack, StackProps } from "aws-cdk-lib";
-import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { GatewayVpcEndpointAwsService, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 
 export class NetworkingStack extends Stack {
@@ -12,6 +12,12 @@ export class NetworkingStack extends Stack {
     this.vpc = new Vpc(this, "Claims-Processor-VPC", {
       maxAzs: 3, // default is all AZs in region
       natGateways: 1, // default is one NAT gateway per AZ
+    });
+
+    this.vpc.addGatewayEndpoint("S3Endpoint", {
+      service: GatewayVpcEndpointAwsService.S3,
+      // Optional: specify route tables (CDK adds to all private subnets by default)
+      // routeTables: [myRouteTable],
     });
   }
 }
